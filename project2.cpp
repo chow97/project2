94f6440814110564caed7a277cfcdfa4e735a2da
Chow <kahoo.chow@syccuxas01.pcc.edu>
2020-01-29 18:18:52 -0800

    Third Try

--- project2.cpp
+++ project2.cpp
@@ -19,10 +19,39 @@ Write an interactive text based menu interface (using a loop) that will allow th
 	Remove a song by index
 	Search forsongs by certain artist
 	Search for songs by a certain album
-	quit
+	Quit
 */
 
+/*
+For each song, you need to keep track of:
 
+	Title
+	Artist
+	duration
+	album
+
+Allow the program to keep looping until user wants to quit. 
+When the program starts, it should load the tasks from external file ("songs.txt") into memory.
+When user enters information about the new song, the program needs to read them in, save them in memory and eventually write them to external data file("songs.txt").
+The file format could look like:
+Stereo Hearts;Gym Class Heroes;3;34;The Papercut Chronicles II 
+Counting stars; OneRepublic;4;17;Native
+
+use ; as delimiter or field separator.
+*/
+
+// Write at least 4 founctions with args
+// Use struct named Sing to model each song
+// Use array of structs to model the collection of songs
+// Hint: Read using the 3 argument version of get.
+// Watch out. When using the 3 argument version of get you need to make sure to remove the delimeter or newline.            Therefore, anytime you read (even confirmation message) make sure to eat the new line using cin.ignore(...)
+// Make sure to have a delimiter written between each item in the file. -- like a new line
+// this will be important when you read the information back from the file.
+
+/*
+For submission your data file should contain a sufficient set of data
+It should have test cases for same artist with multiple songs and same album with multiple songs in it
+*/
 #include <iostream>
 #include <cstring>
 
