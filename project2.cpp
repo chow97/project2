01b9a8ed7f7f35b09d959bde8fa3638dbf888e65
Chow <kahoo.chow@syccuxas01.pcc.edu>
2020-01-29 18:00:44 -0800

    second try

--- project2.cpp
+++ project2.cpp
@@ -1,3 +1,28 @@
+//use character arrays and <cstirng>
+//use struct to model composite data tyoe
+//file input/output
+//use array of objects to model a collection
+//master pass by reference or pass by value
+
+/*
+You are asked to write an app to keep track of relatively small music library.
+The app should load song information from a data file once the app is started. 
+It should allow user to view, add, remove and search for songs. 
+The app should save the data back to the same data file when the program exits.
+*/
+
+/*
+Write an interactive text based menu interface (using a loop) that will allow the user to 
+
+	Enter information for a new song.
+	Display information for all the songs in the database with index for each song
+	Remove a song by index
+	Search forsongs by certain artist
+	Search for songs by a certain album
+	quit
+*/
+
+
 #include <iostream>
 #include <cstring>
 
